f726decd8d2ea4fc4f1a87c406537655fea945df
neahtSan <thaenthai124@gmail.com>
2025-04-21 02:38:12 +0700

    [ADD] Loading screen for blog page

---
+++ frontend/app/blog/loading.tsx
@@ -0,0 +1,9 @@
+// app/blog/loading.tsx
+export default function Loading() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-white">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+      </div>
+    );
+  }
+  
